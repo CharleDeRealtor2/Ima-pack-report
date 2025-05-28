ab1960049383fe5cee1a8387bb3b375d2b2ab4c9
chinstep <steelven1308@gmail.com>
2025-05-28 05:29:16 +0100

    Add react-scripts to dependencies

---
+++ build/precache-manifest.c1c104e34a17ca14582a0c05f326abe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83611ab6127fa8f84c95d06d96dd4a65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "778fa8d922f4af67f759",
+    "url": "./static/css/main.c9faaa63.chunk.css"
+  },
+  {
+    "revision": "57c47cbefb989e2f651f",
+    "url": "./static/js/2.b1b8e45f.chunk.js"
+  },
+  {
+    "revision": "27e5feaedc2e0df8b03bfc4072862604",
+    "url": "./static/js/2.b1b8e45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778fa8d922f4af67f759",
+    "url": "./static/js/main.623032b8.chunk.js"
+  },
+  {
+    "revision": "89c7ba98a8bb97113c41",
+    "url": "./static/js/runtime-main.fbd49afa.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bf754da64f0aeebfc99a7b393872ceac.js"
+  "./precache-manifest.c1c104e34a17ca14582a0c05f326abe2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
